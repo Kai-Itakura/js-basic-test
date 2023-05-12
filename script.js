e86ff4d6657644cb7029764ca9d25bb8eb9eaf16
Kai-Itakura <itakai199969@gmail.com>
2023-05-12 23:32:23 +0900

    さらに問題を追加

--- script.js
+++ script.js
@@ -18,3 +18,24 @@ const person = {
   age: 23,
 }
 console.log(person.name)
+
+// 問題４
+const str = 'free research preview ChatGPT may produce inaccurate information about people, places, or facts.'
+const arr = str.split(' ')
+console.log(arr.map((word) => word.charAt(0).toUpperCase() + word.slice(1)).join(' '))
+
+// 問題５
+const createRange = (a, b) => {
+  const start = Math.min(a, b)
+  const end = Math.max(a, b)
+  const array = Array.from({ length: end - start + 1 }, (_, i) => start + i)
+  console.log(array)
+}
+createRange(20, 7)
+
+// 問題６
+const num = [1, 2, 3, 4, 5, 6, 6, 67, 7, 8, 7, 6, 4, 43]
+const findMax = (num) => {
+  return num.reduce((max, num) => (num > max ? num : max))
+}
+console.log(findMax(num))
