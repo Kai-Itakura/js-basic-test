dbe2d678aa083a4180a64c50948e34942a792465
Kai-Itakura <itakai199969@gmail.com>
2023-05-16 09:59:14 +0900

    今日の学習内容

--- script.js
+++ script.js
@@ -109,8 +109,188 @@
 // console.log(upperCaseStr(`i'm kai itakura!`))
 
 /* 問題１５ */
-const convertDate = (timeStr) => {
-  const parts = timeStr.match(/\d+/g)
-  return new Date(parts[0], parts[1] - 1, parts[2], parts[3], parts[4], parts[5])
+// const convertDate = (timeStr) => {
+//   const parts = timeStr.match(/\d+/g)
+//   return new Date(parts[0], parts[1] - 1, parts[2], parts[3], parts[4], parts[5])
+// }
+// console.log(convertDate('1999/06/09 12:56:43'))
+
+/* 問題１６ */
+// const createNumbers = (num) => {
+//   return Array.from({ length: num }, (_, i) => i + 1)
+// }
+// console.log(createNumbers(9))
+
+/* 問題１７ */
+// const reverseStr = (s) => {
+//   return s.split('').reverse().join('')
+// }
+// console.log(reverseStr('Kai Itakura'))
+
+/* 問題１８ */
+// const calcSum = (arr) => {
+//   return arr.reduce((sum, current) => sum + current)
+// }
+// console.log(calcSum([1, 2, 3, 4, 5, 5, 67, 7]))
+
+/* 問題１９ */
+// const stringCount = (str) => {
+//   return str.length
+// }
+// console.log(stringCount('Kai'))
+
+/* 問題２0 */
+// const mapArray = (arr, func) => {
+//  return arr.map(funk)
+// }
+
+/* 問題２１ */
+// const filterArray = (arr, funk) => {
+//   return arr.filter(funk)
+// }
+
+/* 問題２３ */
+// const reduceArray = (arr) => {
+//   return arr.reduce((acc, val) => acc + val, 0)
+// }
+// console.log(reduceArray([3, 4, 4, 4, 5, 64, 56, 456, 345, 3, 123, 14]))
+
+/* 問題24 */
+// const divide = (a, b) => a % b
+// console.log(divide(82, 8))
+
+/* 問題25 */
+// const setArray = (arr) => {
+//   return new Set(arr)
+// }
+// console.log(setArray(['set', 'kai', 'koki', 'kai', 'koike', 'makoto', 'set', 'satoshi', 'satosi', 'koike']))
+
+/* 問題26 */
+// const createNumbers = (n) => {
+//   return Array.from({ length: n }, (_, i) => i + 1)
+// }
+// console.log(createNumbers(7))
+
+/* 問題27 */
+// const greatestCommonDivisor = (a, b) => {
+//   if (b === 0) {
+//     return a
+//   } else {
+//     return greatestCommonDivisor(b, a % b)
+//   }
+// }
+// console.log(greatestCommonDivisor(0, 56))
+
+/* 問題28 */
+// const createNumArray = (arr, n) => {
+//   return arr.slice(0, n)
+// }
+// console.log(createNumArray(['kai', 'musashi', 'reiki', 'kamui', 'taishi', 'ryota', 'takeru', 'hikaru', 'tomohiro'], 30))
+
+/* 問題29 */
+// const arrayFromTwoArray = (arr1, arr2) => {
+//   return arr1.filter((elem) => arr2.includes(elem))
+// }
+// console.log(arrayFromTwoArray(['kai', 'himika', 'mushishiko'], ['kai', 'himika', 'takeshi']))
+
+/* 問題30 */
+// const capitalize = (s) => {
+//   if (s.length === 1) {
+//     return s.toUpperCase()
+//   } else {
+//     return s.charAt(0).toUpperCase() + s.slice(1, -1) + s.slice(-1).toUpperCase()
+//   }
+// }
+// console.log(capitalize('hello world'))
+
+/* 問題31 */
+// const replaceObj = (obj) => {
+//   return Object.entries(obj).reduce((acc, [key, value]) => {
+//     acc[value] = key
+//     return acc
+//   }, {})
+// }
+// console.log(replaceObj({ name: 'kai', age: 23, job: 'frontend' }))
+
+/* 問題32 */
+// const sortDescending = (arr) => {
+//   return arr.sort((a, b) => b - a)
+// }
+// console.log(sortDescending(['2', '4', '66', '7', '1']))
+
+/* 問題33 */
+// const swapFirstAndLast = (arr) => {
+//   return [arr[arr.length - 1], ...arr.slice(1, -1), arr[0]]
+// }
+// console.log(swapFirstAndLast(['koba', 'saiko', 'tecchan', 'doba', 'beckham']))
+
+/* 問題34 */
+// const isEven = (arr) => {
+//   return arr.map((elem) => (elem % 2 === 0 ? true : false))
+// }
+// console.log(isEven([1, 2, 3, 4, 4, 5, 6, 6, 7, 7, 8, 8, 9, 10]))
+
+/* 問題35 */
+// const newSetArray = (arr) => {
+//   return new Set(arr)
+// }
+// console.log(newSetArray(['koki', 'horoto', 'subaru', 'koki', 'chihoro', 'subaru']))
+
+/* 問題36 */
+// const doubleArray = (arr) => {
+//   return arr.map((elem) => elem * 2)
+// }
+// console.log(doubleArray([1, 2, 3, 4, 5, 5, 6, 7, 7, 8, 9]))
+
+/* 問題37 */
+// const getMaximumNum = (arr) => {
+//   return Math.max(...arr)
+// }
+// console.log(getMaximumNum([1, 23, 5, 45, 56, 4, 7, 8, 9, 78, 6, 856, 7, 46, 3]))
+
+/* 問題38 */
+// const matchNum = (arr) => {
+//   const num = 4
+//   return arr.filter((elem) => elem !== num)
+// }
+// console.log(matchNum([1, 2, 3, 4, 5, 6, 7, 8, 9, 4]))
+
+/* 問題39 */
+// const swapFirstAndLast = (arr) => {
+//   if (arr.length < 2) {
+//     return arr
+//   } else {
+//     const first = arr[0]
+//     const last = arr.slice(-1)[0]
+//     return [last, ...arr.slice(1, -1), first]
+//   }
+// }
+// console.log(swapFirstAndLast(['himi', 'kai', 'reiki', 'takashi', 'bono']))
+
+/* 問題40 */
+// const countOccurrences = (arr) => {
+//   const num = 7
+//   return arr.filter((elem) => elem === num).length
+// }
+// console.log(countOccurrences([1, 2, 3, 4, 5, 6, 7, 7, 8, 0, 7, 6, 8, 7, 9, 8]))
+
+/* 問題41 */
+// const FizzBuzz = () => {
+//   for (i = 1; i <= 100; i++) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//       console.log('FizzBuzz')
+//     } else if (i % 3 === 0) {
+//       console.log('Fizz')
+//     } else if (i % 5 === 0) {
+//       console.log('Buzz')
+//     } else {
+//       console.log(i)
+//     }
+//   }
+// }
+// FizzBuzz()
+
+/* 問題42 */
+const findMostFrequent = (arr) => {
+  const frequencyMap = arr.reduce()
 }
-console.log(convertDate('1999/06/09 12:56:43'))
